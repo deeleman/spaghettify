7d95104ba3c8668e6d09765e0560a1146c5a6609
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2021-05-20 00:19:34 +0200

    feat: Add routeFilter  middleware

--- src/middleware/route-filter/route-filter.ts
+++ src/middleware/route-filter/route-filter.ts
@@ -1,7 +1,17 @@
 import { MiddlewareHandler, MiddlewarePayload } from 'spaghettify/core/stream-writer'
 
+/**  */
 export const routeFilter = (routes: string[]): MiddlewareHandler => {
-  return (payload: MiddlewarePayload): MiddlewarePayload => {
-    return payload;
+  const routeRegexPattern = routes.join('|')
+    .replace(/\//g, '\\/')
+    .replace(/\./g, '\\.')
+    .replace(/\+/g, '\\+')
+    .replace(/\?/g, '\\?')
+    .replace(/\*/g, '.*');
+
+  const regex = new RegExp(routeRegexPattern, 'gi');
+
+  return (payload: MiddlewarePayload): MiddlewarePayload | undefined => {
+    return regex.test(payload.anchor.href) ? payload : void 0;
   };
 };
