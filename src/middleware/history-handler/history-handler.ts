5d0f57b55ab1ca344c4f71c536459f90edcff667
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2021-05-24 18:22:15 +0200

    chore: Remove loggin snippet

--- src/middleware/history-handler/history-handler.ts
+++ src/middleware/history-handler/history-handler.ts
@@ -2,14 +2,6 @@ import { MiddlewareHandler, MiddlewarePayload } from 'spaghettify/core/stream-wr
 
 export const historyHandler = (): MiddlewareHandler => {
   return (payload: MiddlewarePayload): MiddlewarePayload => {
-
-    // TODO: Replace this mock data by real implementation
-    const data = document.createElement('div');
-    data.innerHTML = '<h1>JavaScript DOM</h1>';
-
-    return {
-      ...payload,
-      data,
-    };
+    return payload;
   };
 };
