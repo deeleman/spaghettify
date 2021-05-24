92ec88a152ec3e4e545113778991ecbb6e270f93
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2021-05-24 18:28:39 +0200

    feat: Implement web scrapping middleware

--- src/middleware/web-scraper/web-scraper.ts
+++ src/middleware/web-scraper/web-scraper.ts
@@ -1,7 +1,22 @@
-import { MiddlewareHandler, MiddlewarePayload } from 'spaghettify/core/stream-writer'
+import { MiddlewareHandler, MiddlewarePayload, httpClient } from 'spaghettify/core'
 
+/**
+ * 
+ * @param enableProgressBar 
+ * @returns 
+ */
 export const webScraper = (enableProgressBar?: boolean): MiddlewareHandler => {
-  return (payload: MiddlewarePayload): MiddlewarePayload => {
-    return payload;
+  const responseDOMParser = new DOMParser();
+  const responseDOMSerializer = (responseText: string) => responseDOMParser.parseFromString(responseText, 'text/html');
+
+  return async (payload: MiddlewarePayload): Promise<MiddlewarePayload> => {
+    const { href } = payload.anchor;
+    const scrapedPageDOM = await httpClient<Document>(href, responseDOMSerializer);
+
+    return {
+      ...payload,
+      rawData: scrapedPageDOM,
+      data: scrapedPageDOM.body,
+    };
   };
 };
