070437be3a4ec152b70fd0855e0fe65b2c670883
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2021-06-01 20:59:35 +0200

    chore: Add missing JSDoc to type

--- src/core/events-listener/events-listener.types.ts
+++ src/core/events-listener/events-listener.types.ts
@@ -1,16 +1,16 @@
 /**  */
 export type AnchorEvent = globalThis.MouseEvent | globalThis.TouchEvent;
 
-/** */
+/** Settings object for the EventsListener class  */
 export type EventsListenerSettings = {
-  /** */
+  /** DOM element (usually `document.body`) to scan for elements matching `selector`. */
   element: HTMLElement;
-  /** */
+  /** Event type to listen to, as formalized by the `HTMLElementEventMap` union type. */
   elementEvent: keyof HTMLElementEventMap;
-  /** */
+  /** CSS selector to lookup elements within `element`. */
   selector: string;
 };
 
-/**  */
+/** Typed event listener callback */
 export type EventCallback = (anchor: HTMLAnchorElement, event: AnchorEvent) => void;
 
--- src/core/progress-bar/progress-bar.types.ts
+++ src/core/progress-bar/progress-bar.types.ts
@@ -1,2 +1,5 @@
-/** */
+/**
+ * A callback function expecting an integer value in its signature 
+ * informing of the current state of download progress
+ */
 export type LoadProgressHandler = (onLoadProgress: number) => void;
